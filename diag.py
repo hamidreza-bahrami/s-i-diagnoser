e5053cce8402c85e0150aa4fdb7e42872e78f123
hamidreza-bahrami <150763463+hamidreza-bahrami@users.noreply.github.com>
2024-03-19 20:20:38 +0330

    Update diag.py

--- diag.py
+++ diag.py
@@ -3,7 +3,7 @@ import pickle
 import re
 import numpy as np
 import nltk
-from nltk.corpus import stopwords
+nltk.download('stopwords')
 from nltk.stem.porter import PorterStemmer
 from sklearn.feature_extraction.text import TfidfVectorizer
 import time
